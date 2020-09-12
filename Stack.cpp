f0e7cd557aa43590f90eecbfc207fcd21dc41cb0
Sakshi Joshi <64726139+Sakshi0401@users.noreply.github.com>
2020-09-12 21:39:56 +0530

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -16,7 +16,7 @@ int main()
 {
 	cout<<"Program to implement stack in C++."<<endl;
 	do{
-		cout<<endl<<"1.Print stack  2.Push  3.Pop  4.Exit"<<endl<<"Enter choice"<<endl;
+		cout<<endl<<"1.Print stack  2.Push  3.Pop  4.size of stack  5.Exit"<<endl<<"Enter choice"<<endl;
 		cin>>ch;	//enter choice
 		switch(ch)
 		{
@@ -29,11 +29,18 @@ int main()
 				cin>>x;
 				s.push(x);	//push value of x inside stack
 				break;
-			case 3:
-				s.pop();	//pop value from top of stack
+			case 3: 
+				if(s.empty())		//check if stack is empty
+					cout<<"Stack is empty.";
+				else
+					s.pop();	//pop value from top of stack
 				break;
 			case 4:
-				exit(0);break;	//exit from program
+				cout<<s.size();		//print size of stack
+				break;
+			case 5:
+				exit(0);		//exit from program
+				break;	
 				
 		}	
 	
